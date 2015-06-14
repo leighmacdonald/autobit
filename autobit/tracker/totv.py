a79225d12013bb7445b9dee38af8a867affafa79
Leigh MacDonald <leigh.macdonald@gmail.com>
2015-06-14 00:47:09 -0600

    totv download functional

--- autobit/tracker/totv.py
+++ autobit/tracker/totv.py
@@ -4,6 +4,7 @@
 """
 from __future__ import unicode_literals, absolute_import
 import requests
+from autobit import config
 from autobit.classification import MediaType
 from autobit.db import Release
 from autobit.tracker import Tracker
@@ -13,11 +14,17 @@ class TitansOfTV(Tracker):
     announce_url = 'http://tracker.titansof.tv:34000/{}/announce'
     upload_endpoint = 'http://titansof.tv/api/torrents/upload'
 
+    def __init__(self):
+        self._apikey = ""
+        super().__init__()
+
     def reconfigure(self):
-        pass
+        self._apikey = config["TOTV_APIKEY"]
 
     def download(self, release: Release) -> bytes:
-        pass
+        url = "https://titansof.tv/api/torrents/{}/download".format(release.torrent_id)
+        torrent_file = self._fetch(url, params={"apikey": self._apikey})
+        return torrent_file
 
     def upload(self, release_name, torrent_file) -> bool:
         payload = {
