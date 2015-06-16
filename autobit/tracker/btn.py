5214e1ba32c13872c15df6e02054f97adbebf521
Leigh MacDonald <leigh.macdonald@gmail.com>
2015-06-16 00:53:34 -0600

    btn parser working

--- autobit/tracker/btn.py
+++ autobit/tracker/btn.py
@@ -4,18 +4,24 @@
 """
 from __future__ import unicode_literals, absolute_import
 from autobit import config
+from autobit import ConfigError
 from autobit.tracker import Tracker
 from autobit.db import Release
+from classification import MediaType, MediaClass
 
 
 class BroadcastTheNet(Tracker):
     def __init__(self):
+        self.name = "btn"
         self._authkey = ""
         self._passkey = ""
         super().__init__()
 
     def parse_line(self, message: str) -> Release:
-        raise NotImplementedError()
+        pcs = message.split(" | ")
+        rls_name = pcs[-1]
+        torrent_id = pcs[10]
+        return Release(rls_name, torrent_id, MediaType.EPISODE, MediaClass.TV_HD, self.name)
 
     def download(self, release: Release) -> bytes:
         url = "https://broadcasthe.net/torrents.php?action=download&id={}&authkey={}&torrent_pass={}".format(
@@ -24,12 +30,15 @@ class BroadcastTheNet(Tracker):
         return self._fetch(url)
 
     def reconfigure(self):
-        self._authkey = config['BTN_AUTHKEY']
-        self._passkey = config['BTN_PASSKEY']
-        if all([self._authkey, self._passkey, len(self._authkey) == 32, len(self._passkey) == 32]):
-            self.enable()
-        else:
+        try:
+            self._authkey = config['BTN_AUTHKEY']
+            self._passkey = config['BTN_PASSKEY']
+            if all([self._authkey, self._passkey, len(self._authkey) == 32, len(self._passkey) == 32]):
+                raise ConfigError()
+        except (KeyError, ConfigError):
             self.disable()
+        else:
+            self.enable()
 
     def upload(self, release_name, torrent_file) -> bool:
         raise NotImplementedError()
